0ed6c88861ef486b6da2409a71650ebeb1c5a4ef
Wes Young <wes@barely3am.com>
2015-04-07 15:04:40 -0400

    adding group flag

--- contrib/fetch_apwg_feeds.sh
+++ contrib/fetch_apwg_feeds.sh
@@ -4,5 +4,6 @@ set -e
 
 # this should go in your env...
 #APWG_TOKEN="12341234"
+GROUP="everyone"
 
-/usr/local/bin/cif-apwg -v --apwg-token $APWG_TOKEN
+/usr/local/bin/cif-apwg -v --apwg-token $APWG_TOKEN --group $GROUP
